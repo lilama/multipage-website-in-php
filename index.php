e3774a6a1a61b1c4c15d4776525216654ed804d8
CharlotteTusset <charlotte.tusset@gmail.com>
2018-07-30 12:11:42 +0200

    Structure

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>La Chôm'hier</title>
+
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" href="http://www.chomhier.be/images/logoChomhierReduit2.png">
+
+		<!-- Framework CSS - Bulma -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.css">
+	</head>
+	<body>
+		<h1>Hello, world!</h1>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first-->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	</body>
+</html>
